5bcfbe26a6a7bc1b22e8ea47cdec17983b77b1da
hop-sys <hope28224@gmail.com>
2026-03-12 14:05:16 +0300

    added codes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,6 @@
+import axios from 'axios';
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { data, Link } from 'react-router-dom';
 
 const Signup = () => {
   // initialize the hook(they help us manage states of an application)
@@ -7,12 +8,63 @@ const Signup = () => {
   const[email, setEmail] = useState("");
   const[password, setPassword] = useState("");
   const[phone, setPhone] =useState("");
+
+  // Dfine the three states the application will go through
+  const[loading, setLoading] = useState("");
+  const[success, setSuccess] = useState("");
+  const[error, setError] = useState("");
+
+  // below is a function that will handle the submit action
+  const handleSubmit = async(e) =>{
+    // below we prevent our site from reloading
+    e.preventDefault()
+
+    // update our loading hook with a message that will be displayed to the users who are trying to register
+    setLoading("Your request is being processed...")
+
+    try{
+      // create a form data object that will enable you to capture the four details entered on the form
+      const formdata = new FormData();
+
+      // insert the four details(username, email, password, phone) in terms of key-value pairs
+      formdata.append("username", username);
+      formdata.append("email", email);
+      formdata.append("password", password);
+      formdata.append("phone", phone);
+
+      // by use of axios, we can access the method POST
+      const response =await axios.post("https://hope.alwaysdata.net/api/signup", formdata)
+
+      // set back the loading hook to default
+      setLoading("");
+
+      // just incase everything goes on well, update the success hook with a message
+      setSuccess(response.data.message)
+
+      // clear your hooks
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setPhone("");
+    }
+    catch(error){
+      // set the loading hook back to default
+      setLoading("");
+
+      // update the error hook with the message given back from the response
+      setError(error.message)
+    }
+  }
+
   return (
     <div className='row justify-content-center mt-4'>
         <div className="card col-md-6 shadow p-4">
           <h1 className='text-primary'>Sign Up</h1>
 
-          <form>
+          <h5 className="text-warning">{loading}</h5>
+          <h3 className='text-success'>{success}</h3>
+          <h4 className="text-danger">{error}</h4>
+          <form onSubmit={handleSubmit}>
             <input type="text"
             placeholder='Enter the username'
             className='form-control'
@@ -50,7 +102,7 @@ const Signup = () => {
 
             {/* {phone} */}
 
-            <input type="button" value="Signup" className='btn btn-primary'/>
+            <input type="submit" value="Signup" className='btn btn-primary'/>
             <br /><br />
 
             Already have an account? <Link to={'/signin'}>Signin</Link>
